23c4fe36bbc32ab7f308b57f62d5d8266dd7f50a
Nuffily <hehehe>
2024-10-28 22:43:48 +0500

    version 1.28

--- src/main/java/PrintService.java
+++ src/main/java/PrintService.java
@@ -2,7 +2,7 @@ public class PrintService {
 
     static ResourceStorage storage;
 
-    public static void printQuote(String command) {
+    public static void printlnQuote(String command) {
 
         String quote = storage.singleQuotes.get(command);
 
@@ -13,7 +13,22 @@ public class PrintService {
         if (quote != null) System.out.println(quote);
     }
 
+    public static void printQuote(String command) {
+
+        String quote = storage.singleQuotes.get(command);
+
+        if ((quote == null) && (storage.randomQuotes.get(command) != null)) {
+            quote = MyUtils.getRandomElement(storage.randomQuotes.get(command));
+        }
+
+        if (quote != null) System.out.print(quote);
+    }
+
     public static void println(String quote) {
         System.out.println(quote);
+    }
+
+    public static void print(String quote) {
+        System.out.print(quote);
     }
 }
