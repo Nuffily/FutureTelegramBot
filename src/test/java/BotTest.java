872061adddb1c3d9d8dc720920d20767cb5f6bd6
orelkrylatiy <maxim700a@gmail.com>
2024-10-30 23:50:45 +0500

    update: rename classes and add some tests

--- src/test/java/BotTest.java
+++ src/test/java/BotTest.java
@@ -12,49 +12,6 @@ public class BotTest {
     private InputService mockInputService;
     private OutputLibrary library;
 
-//    @BeforeEach
-//    public void setup() {
-//        // Создаём заглушку для inputService
-//        mockInputService = mock(InputService.class);
-//
-//        // Создаём и настраиваем объект OutputLibrary
-//        library = new OutputLibrary();
-//        library.commands = new HashMap<>();
-//        library.randomQuotes = new HashMap<>();
-//        library.singleQuotes = new HashMap<>();
-//
-//        // Настраиваем команду и ответ для /start
-//        Map<String, String> mainCommands = new HashMap<>();
-//        mainCommands.put("/start", "startCommand");
-//        library.commands.put("main", mainCommands);
-//
-//        library.singleQuotes.put("startCommand", "Привет! Я бот.");
-//
-//        // Создаём бота и передаём ему моки
-//        bot = new Bot();
-//        bot.inputService = mockInputService;
-//        bot.library = library;
-//    }
-//
-//    @Test
-//    public void testStartCommandOutput() {
-//        // Настраиваем mock так, чтобы он возвращал "/start" при вызове inputService.readLine()
-//        when(mockInputService.readLine()).thenReturn("/start");
-//
-//        // Перехватываем вывод в консоль
-//        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(outContent));
-//
-//        // Запускаем метод run, который должен вызвать команду "/start"
-//        bot.run();
-//
-//        // Проверяем, что в консоли был выведен ожидаемый результат
-//        assertEquals("Привет! Я бот.\n", outContent.toString());
-//
-//        // Восстанавливаем стандартный вывод в консоль
-//        System.setOut(System.out);
-//    }
-
     @Test
     public void testExecute_travelToJS() {
         Bot bot = new Bot();
