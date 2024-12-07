66de065349f9299aa158a75ae62e9c9afe332bce
Nuffily <hehehe>
2024-12-07 17:02:45 +0500

    1.945 - Никаких звездочек

--- src/test/java/QuestionStatisticsTest.java
+++ src/test/java/QuestionStatisticsTest.java
@@ -12,7 +12,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class QuestionStatisticsTest {
