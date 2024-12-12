d7f010eae4ede026995868bdb2d71bb3d83d7f93
nuffily <hehehe@>
2024-12-12 11:49:22 +0500

    Работаем #11

--- src/main/java/bot/console/Bot.java
+++ src/main/java/bot/console/Bot.java
@@ -116,8 +116,7 @@ public class Bot implements Runnable {
                         "repeatSolved " + toONorOFF(!settings.getRepeatSolved()),
                         "showAnswer " + toONorOFF(!settings.getShowAnswer()),
                         "showExplanation " + toONorOFF(!settings.getShowExplanation()),
-                        "Назад"
-                        );
+                        "Назад");
                 break;
 
         }
