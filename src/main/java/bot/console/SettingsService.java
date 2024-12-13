6dc4c5bcb89a46b13098c7b541fce024d66f02ff
Nuffily <147496435+Nuffily@users.noreply.github.com>
2024-12-13 16:33:45 +0500

    Доработка второй задачи
    Длинные switch'и заменены на map'ы

--- src/main/java/bot/console/SettingsService.java
+++ src/main/java/bot/console/SettingsService.java
@@ -1,5 +1,10 @@
 package bot.console;
 
+import model.Location;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SettingsService {
 
     private boolean repeatQuestions = true;
@@ -7,47 +12,62 @@ public class SettingsService {
     private boolean showAnswer = true;
     private boolean showExplanation = false;
     private final OutputService printer;
+    private final Map<String, Runnable> settingMap;
 
-    public SettingsService(OutputService printer) {
+    public SettingsService(OutputService printer)
+    {
         this.printer = printer;
+        settingMap = createSettingMap();
     }
 
     public void settingsChanger(String command) {
-        switch (command) {
-            case "repeatON":
-                repeatQuestions = true;
-                printer.println("встречавшиеся вопросы доступны");
-                break;
-            case "repeatOFF":
-                repeatQuestions = false;
-                printer.println("встречавшиеся вопросы скрыты");
-                break;
-            case "repeatSolvedON":
-                repeatSolved = true;
-                printer.println("верно решенные вопросы доступны");
-                break;
-            case "repeatSolvedOFF":
-                repeatSolved = false;
-                printer.println("верно решенные вопросы скрыты");
-                break;
-            case "showAnswerON":
-                showAnswer = true;
-                printer.println("при неверном решении виден ответ");
-                break;
-            case "showAnswerOFF":
-                showAnswer = false;
-                printer.println("при неверном решении ответ не выводится");
-                break;
-            case "showExplanationON":
-                showExplanation = true;
-                printer.println("при неверном решении выводится объяснение");
-                break;
-            case "showExplanationOFF":
-                showExplanation = false;
-                printer.println("при неверном решении объяснения не будет");
-                break;
-            default:
-        }
+        if (settingMap.containsKey(command)) { settingMap.get(command).run(); }
+    }
+
+    private Map<String, Runnable> createSettingMap(){
+        Map<String, Runnable> Map = new HashMap<>();
+
+        Map.put("repeatON", () -> {
+            repeatQuestions = true;
+            printer.println("встречавшиеся вопросы доступны");
+        });
+
+        Map.put("repeatOFF", () -> {
+            repeatQuestions = false;
+            printer.println("встречавшиеся вопросы скрыты");
+        });
+
+        Map.put("repeatSolvedON", () -> {
+            repeatSolved = true;
+            printer.println("верно решенные вопросы доступны");
+        });
+
+        Map.put("repeatSolvedOFF", () -> {
+            repeatSolved = false;
+            printer.println("верно решенные вопросы скрыты");
+        });
+
+        Map.put("showAnswerON", () -> {
+            showAnswer = true;
+            printer.println("при неверном решении виден ответ");
+        });
+
+        Map.put("showAnswerOFF", () -> {
+            showAnswer = false;
+            printer.println("при неверном решении ответ не выводится");
+        });
+
+        Map.put("showExplanationON", () -> {
+            showExplanation = true;
+            printer.println("при неверном решении выводится объяснение");
+        });
+
+        Map.put("showExplanationOFF", () -> {
+            showExplanation = false;
+            printer.println("при неверном решении объяснения не будет");
+        });
+
+        return Map;
     }
 
     public boolean getRepeatQuestions() {
